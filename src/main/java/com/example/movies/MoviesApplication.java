e0ee3f8d16aafa8cf826193ef1a2dca29311fe11
Nikhil Wadhwa <101242510+NikhilWadhwa77@users.noreply.github.com>
2024-02-05 13:01:08 +0530

    Update MoviesApplication.java

--- src/main/java/com/example/movies/MoviesApplication.java
+++ src/main/java/com/example/movies/MoviesApplication.java
@@ -2,14 +2,9 @@ package com.example.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class MoviesApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
